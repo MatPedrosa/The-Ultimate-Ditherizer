f5d8f8c07e07ed9ad32c5ad2b5340dc49ac7b818
MatPedrosa <mat.pedrosa@proton.me>
2026-05-14 03:20:55 -0300

    Added palette

--- Ditherizer.py
+++ Ditherizer.py
@@ -2,21 +2,36 @@ from PIL import Image
 import numpy as np
 import colour
 
-input_image = Image.open("USC-SIPI-4.2.06.tiff").convert("RGB")
-input_sRGB = np.asarray(input_image)
-input_Oklab = colour.XYZ_to_Oklab(colour.sRGB_to_XYZ(input_sRGB / 255))
+def sRGB_to_Oklab(color):
+    return colour.XYZ_to_Oklab(colour.sRGB_to_XYZ(color / 255))
 
-def OklabDistance(color1, color2):
+def Oklab_to_sRGB(color):
+    return 255 * colour.XYZ_to_sRGB(colour.Oklab_to_XYZ(color))
+
+def OklabSquaredDistance(color1, color2):
     diff = color1 - color2
+    #calculating the actual distance would need a square root
+    #but we don't need that if we're just comparing distances
     return np.dot(diff, diff)
 
-output_image = Image.new(mode="1", size = input_image.size)
-output_pixels = output_image.load()
-for j in range(input_image.height):
-    for i in range(input_image.width):
-        if OklabDistance(input_Oklab[j, i, 0], np.array([0, 0, 0])) > OklabDistance(input_Oklab[j, i, 0], np.array([1.00000174e+00, 2.28547958e-06, -1.13652666e-04])):
-            output_pixels[i, j] = 1
-        else:
-            output_pixels[i, j] = 0
+def OklabClosestColor(color, palette):
+    distances = np.array([OklabSquaredDistance(color, item) for item in palette])
+    sorted_indices = np.argsort(distances)
+    return palette[sorted_indices[0]], sorted_indices
 
+
+input_image = Image.open("C:\\Users\\mathe\\Documentos\\Projects\\ditherizer\\USC-SIPI-4.2.06.tiff").convert("RGB")
+input_sRGB = np.asarray(input_image)
+input_Oklab = sRGB_to_Oklab(input_sRGB)
+
+color_palette_sRGB = np.asarray([[0, 0, 0], [255, 0, 0], [0, 255, 0], [0, 0, 255], [0, 255, 255], [255, 0, 255], [255, 255, 0], [255, 255, 255]])
+color_palette_Oklab = sRGB_to_Oklab(color_palette_sRGB)
+
+output_Oklab = np.empty_like(input_Oklab)
+for i, j in np.ndindex(input_Oklab.shape[:2]):
+    output_Oklab[j, i] = tuple(OklabClosestColor(input_Oklab[j, i], color_palette_Oklab)[0])
+    #if j == 0 : print(output_Oklab[i, j])
+
+output_sRGB = Oklab_to_sRGB(output_Oklab)
+output_image = Image.fromarray(output_sRGB.astype('uint8'))
 output_image.show()
