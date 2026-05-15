6023da210191162ad4fd93a7d525a4c218087645
MatPedrosa <mat.pedrosa@proton.me>
2026-05-15 03:36:41 -0300

    Test Floyd-Steinberg implementation

--- Ditherizer.py
+++ Ditherizer.py
@@ -14,23 +14,43 @@ def OklabSquaredDistance(color1, color2):
     #but we don't need that if we're just comparing distances
     return np.dot(diff, diff)
 
-def OklabClosestColor(color, palette):
+def OklabQuantize(color, palette):
     distances = np.array([OklabSquaredDistance(color, item) for item in palette])
     sorted_indices = np.argsort(distances)
     return palette[sorted_indices[0]], sorted_indices
 
 
-input_image = Image.open("C:\\Users\\mathe\\Documentos\\Projects\\ditherizer\\USC-SIPI-4.2.06.tiff").convert("RGB")
+input_image = Image.open("Teste1.png").convert("RGB")
 input_sRGB = np.asarray(input_image)
 input_Oklab = sRGB_to_Oklab(input_sRGB)
+#For some unknown reason, these image arrays are adderessed in the order (y, x)
 
-color_palette_sRGB = np.asarray([[0, 0, 0], [255, 0, 0], [0, 255, 0], [0, 0, 255], [0, 255, 255], [255, 0, 255], [255, 255, 0], [255, 255, 255]])
+color_palette_sRGB = np.asarray([
+    [0, 0, 0],
+    [255, 0, 0],
+    [0, 255, 0],
+    [0, 0, 255],
+    [0, 255, 255],
+    [255, 0, 255],
+    [255, 255, 0],
+    [255, 255, 255]
+])
 color_palette_Oklab = sRGB_to_Oklab(color_palette_sRGB)
 
 output_Oklab = np.empty_like(input_Oklab)
-for i, j in np.ndindex(input_Oklab.shape[:2]):
-    output_Oklab[j, i] = tuple(OklabClosestColor(input_Oklab[j, i], color_palette_Oklab)[0])
-    #if j == 0 : print(output_Oklab[i, j])
+for j in range(input_image.height):
+    for i in range(input_image.width):
+        #Test Floyd-Steinberg implementation
+        output_Oklab[j, i] = tuple(OklabQuantize(input_Oklab[j, i], color_palette_Oklab)[0])
+        current_error = output_Oklab[j, i] - input_Oklab[j, i]
+        if i + 1 < input_image.width:
+            input_Oklab[j, i + 1] -= 7 * current_error / 16
+        if i + 1 < input_image.width and j + 1 < input_image.height:
+            input_Oklab[j + 1, i + 1] -= current_error / 16
+        if j + 1 < input_image.height:
+            input_Oklab[j + 1, i] -= 5 * current_error / 16
+        if i - 1 < input_image.width and j + 1 < input_image.height:
+            input_Oklab[j + 1, i - 1] -= 3 * current_error / 16
 
 output_sRGB = Oklab_to_sRGB(output_Oklab)
 output_image = Image.fromarray(output_sRGB.astype('uint8'))
